99ba308c5a4c792441ce7d7574b0526273ff72b4
1amos9 <amosthibault19@gmail.com>
2022-05-25 00:50:13 +0200

    security page

--- ROYAL BOOTSTRAP/js/script.js
+++ ROYAL BOOTSTRAP/js/script.js
@@ -58,3 +58,15 @@ function initMap() {
     map: map,
   });
 }
+document.querySelector(".nav_search-btn").style.height = "35px";
+document.querySelector(".nav_search-btn").style.width = "35px";
+document.querySelector(".logo_box").style.width = "80px";
+document.querySelector(".logo_box img").style.width = "80%";
+document.querySelector(".carousel-indicators li").style.width = "5px";
+document.querySelector(".carousel-indicators li").style.height = "30px";
+document.querySelector(".slider-detail").style.width = "60%";
+document.querySelector(".welcome_detail").style.width = "75%";
+document.querySelector(".welcome_detail-btn a img").style.width = "25px";
+document.querySelector(".security_container").style.width = "95%";
+document.querySelector(".security_btn a img").style.width = "25px";
+document.querySelector(".story_detail").style.width = "85%";
